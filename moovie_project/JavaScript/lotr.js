f58c1bffc590e1e7d0e1000325a3f82d6edbb3ab
AnastasiR92 <anastasiya.revinskaya@gmail.com>
2024-08-14 23:51:23 +0300

    add gallery

--- moovie_project/JavaScript/lotr.js
+++ moovie_project/JavaScript/lotr.js
@@ -66,134 +66,141 @@ button.addEventListener("click", async () => {
   }
 });
 
-// Movie gallery with Swiper.js
-document.addEventListener("DOMContentLoaded", function () {
-  const LOTRcardsArray = [
-    {
-      title: "Lord of the Rings: The Fellowship of the Ring",
-      genres: "Action/Adventure, Sci-Fi/Fantasy",
-      year: "2001",
-      url: "./assets/images/LOTR/ring.jpg",
-    },
-    {
-      title: "Lord of the Rings: The Two Towers",
-      genres: "Action/Adventure, Oscar Winners, Sci-Fi/Fantasy",
-      year: "2002",
-      url: "./assets/images/LOTR/towers.jpg",
-    },
-    {
-      title: "The Lord of the Rings: The Return of the King",
-      genres: "Action/Adventure, Oscar Winners, Sci-Fi/Fantasy",
-      year: "2003",
-      url: "./assets/images/LOTR/king.jpg",
-    },
-    {
-      title: "Hobbit: An Unexpected Journey",
-      genres: "Action/Adventure, Sci-Fi/Fantasy",
-      year: "2012",
-      url: "./assets/images/LOTR/journey.jpg",
-    },
-    {
-      title: "The Hobbit: The Desolation of Smaug",
-      genres: "Action/Adventure, Sci-Fi/Fantasy",
-      year: "2013",
-      url: "./assets/images/LOTR/smaug.jpg",
-    },
-    {
-      title: "The Hobbit: The Battle of the Five Armies",
-      genres: "Action/Adventure, Drama, Sci-Fi/Fantasy, Suspense/Thriller",
-      year: "2014",
-      url: "./assets/images/LOTR/armies.jpg",
-    },
-  ];
+// Movie gallery
+const movies = [
+  {
+    title: "Lord of the Rings: The Fellowship of the Ring",
+    genres: "Action/Adventure, Sci-Fi/Fantasy",
+    year: "2001",
+    image: "./assets/images/LOTR/ring.jpg",
+  },
+  {
+    title: "Lord of the Rings: The Two Towers",
+    genres: "Action/Adventure, Oscar Winners, Sci-Fi/Fantasy",
+    year: "2002",
+    image: "./assets/images/LOTR/towers.jpg",
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    genres: "Action/Adventure, Oscar Winners, Sci-Fi/Fantasy",
+    year: "2003",
+    image: "./assets/images/LOTR/king.jpg",
+  },
+  {
+    title: "Hobbit: An Unexpected Journey",
+    genres: "Action/Adventure, Sci-Fi/Fantasy",
+    year: "2012",
+    image: "./assets/images/LOTR/journey.jpg",
+  },
+  {
+    title: "The Hobbit: The Desolation of Smaug",
+    genres: "Action/Adventure, Sci-Fi/Fantasy",
+    year: "2013",
+    image: "./assets/images/LOTR/smaug.jpg",
+  },
+  {
+    title: "The Hobbit: The Battle of the Five Armies",
+    genres: "Action/Adventure, Drama, Sci-Fi/Fantasy, Suspense/Thriller",
+    year: "2014",
+    image: "./assets/images/LOTR/armies.jpg",
+  },
+];
 
-  let currentIndex = 0;
-  const prevButton = document.getElementById("prevButton");
-  const nextButton = document.getElementById("nextButton");
+const lotrWrapper = document.querySelector('.movies-swiper-wrapper');
 
-  setTimeout(() => {
-    updateCard();
-  }, 100);
+movies.forEach(movie => {
+  const lotrSlide = document.createElement('div');
+  lotrSlide.classList.add('swiper-slide');
 
-  const updateCard = () => {
-    const card = LOTRcardsArray[currentIndex];
-    console.log("Updating card:", card);
-    const cardElement = document.querySelector(".card-container");
+  const lotrImg = document.createElement('img');
+  lotrImg.src = movie.image;
+  lotrImg.alt = movie.title;
 
-    if (!cardElement) {
-      console.error("Не найден .card-container элемент!");
-      return;
-    }
+  const lotrTitle = document.createElement('div');
+  lotrTitle.classList.add('movie-title');
+  lotrTitle.textContent = movie.title;
 
-    const imgElement = cardElement.querySelector(".LOTRcard__image");
-    const titleElement = cardElement.querySelector(".LOTRcard__title");
-    const genresElement = cardElement.querySelector(".LOTRcard__genres");
-    const yearElement = cardElement.querySelector(".LOTRcard__year");
+  const lotrGenres = document.createElement('div');
+  lotrGenres.classList.add('movie-genres');
+  lotrGenres.textContent = movie.genres;
 
-    if (!imgElement || !titleElement || !genresElement || !yearElement) {
-      console.error("Не найдены необходимые элементы внутри .card-container!");
-      return;
-    }
+  const lotrYear = document.createElement('div');
+  lotrYear.classList.add('movie-year');
+  lotrYear.textContent = `Published: ${movie.year}`;
 
-    imgElement.src = card.url;
-    imgElement.alt = card.title;
-    titleElement.textContent = card.title;
-    genresElement.innerHTML = `<b>Genres:</b> ${card.genres}`;
-    yearElement.innerHTML = `<b>Year:</b> ${card.year}`;
-  };
+  lotrSlide.appendChild(lotrImg);
+  lotrSlide.appendChild(lotrTitle);
+  lotrSlide.appendChild(lotrGenres);
+  lotrSlide.appendChild(lotrYear);
+  lotrWrapper.appendChild(lotrSlide);
+});
 
-  updateCard();
-
-  prevButton.addEventListener("click", () => {
-    currentIndex =
-      (currentIndex - 1 + LOTRcardsArray.length) % LOTRcardsArray.length;
-    updateCard();
-  });
-
-  nextButton.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % LOTRcardsArray.length;
-    updateCard();
-  });
+const lotrContainer = new Swiper('.movies-swiper', {
+slidesPerView: 3,
+spaceBetween: 30,
+navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+},
+loop: false,
+breakpoints: {
+  640: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+  },
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 30,
+  },
+  1024: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+},
 });
 
 
 const books = [
-  { title: "The Hobbit", year: "1937", image: "/moovie_project/assets/images/LOTR/hobbit.jpg" },
-  { title: "The Fellowship of the Ring", year: "1954", image: "/moovie_project/assets/images/LOTR/fellowship.jpg" },
-  { title: "The Two Towers", year: "1954", image: "/moovie_project/assets/images/LOTR/two_towers.jpg" },
-  { title: "The Return of the King", year: "1955", image: "/moovie_project/assets/images/LOTR/return.jpg" },
-  { title: "The Silmarillion", year: "1977", image: "/moovie_project/assets/images/LOTR/silmarillion.jpg" },
-  { title: "Unfinished Tales of Numenor and Middle-earth", year: "1980", image: "/moovie_project/assets/images/LOTR/unfinished.jpg" },
-  { title: "The Children of Hurin", year: "2007", image: "/moovie_project/assets/images/LOTR/children.jpg" },
-  { title: "Beren and Luthien", year: "2017", image: "/moovie_project/assets/images/LOTR/beren.jpg" },
-  { title: "The Fall of Gondolin", year: "2018", image: "/moovie_project/assets/images/LOTR/fall.jpg" },
+  { title: "The Hobbit", year: "1937", image: "./assets/images/LOTR/hobbit.jpg" },
+  { title: "The Fellowship of the Ring", year: "1954", image: "./assets/images/LOTR/fellowship.jpg" },
+  { title: "The Two Towers", year: "1954", image: "./assets/images/LOTR/two_towers.jpg" },
+  { title: "The Return of the King", year: "1955", image: "./assets/images/LOTR/return.jpg" },
+  { title: "The Silmarillion", year: "1977", image: "./assets/images/LOTR/silmarillion.jpg" },
+  { title: "Unfinished Tales of Numenor and Middle-earth", year: "1980", image: "./assets/images/LOTR/unfinished.jpg" },
+  { title: "The Children of Hurin", year: "2007", image: "./assets/images/LOTR/children.jpg" },
+  { title: "Beren and Luthien", year: "2017", image: "./assets/images/LOTR/beren.jpg" },
+  { title: "The Fall of Gondolin", year: "2018", image: "./assets/images/LOTR/fall.jpg" },
 ];
 
-const swiperWrapper = document.querySelector('.swiper-wrapper');
+const booksWrapper = document.querySelector('.books-swiper-wrapper');
 
         books.forEach(book => {
-            const slide = document.createElement('div');
-            slide.classList.add('swiper-slide');
+            const booksSlide = document.createElement('div');
+            booksSlide.classList.add('swiper-slide');
 
-            const img = document.createElement('img');
-            img.src = book.image;
-            img.alt = book.title;
+            const booksImg = document.createElement('img');
+            booksImg.src = book.image;
+            booksImg.alt = book.title;
 
-            const title = document.createElement('div');
-            title.classList.add('book-title');
-            title.textContent = book.title;
+            const booksTitle = document.createElement('div');
+            booksTitle.classList.add('book-title');
+            booksTitle.textContent = book.title;
 
-            const year = document.createElement('div');
-            year.classList.add('book-year');
-            year.textContent = `Published: ${book.year}`;
+            const booksYear = document.createElement('div');
+            booksYear.classList.add('book-year');
+            booksYear.textContent = `Published: ${book.year}`;
 
-            slide.appendChild(img);
-            slide.appendChild(title);
-            slide.appendChild(year);
-            swiperWrapper.appendChild(slide);
+            booksSlide.appendChild(booksImg);
+            booksSlide.appendChild(booksTitle);
+            booksSlide.appendChild(booksYear);
+            booksWrapper.appendChild(booksSlide);
         });
 
-        const swiper = new Swiper('.swiper-container', {
+        const booksSwiper = new Swiper('.books-swiper', {
           slidesPerView: 3,
           spaceBetween: 30,
           navigation: {
@@ -204,7 +211,7 @@ const swiperWrapper = document.querySelector('.swiper-wrapper');
             el: '.swiper-pagination',
             clickable: true,
           },
-          loop: true,
+          loop: false,
           breakpoints: {
             640: {
               slidesPerView: 1,
